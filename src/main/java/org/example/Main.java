0f1f8b7a910adae2d3555db6119fba574a44310d
Kushagra Agrawal <kushjanhvi@gmail.com>
2023-03-14 10:19:22 +0530

    trial second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 4, b = 5;
+        System.out.print(a+b);
     }
 }
